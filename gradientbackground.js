e390a40feff5b83ebaad5e527f3cc02734aaec5d
Jerry Hong <jerryh2@stanford.edu>
2021-03-21 00:36:57 -0700

    sampling working, bootstrapping MVP

--- gradientbackground.js
+++ gradientbackground.js
@@ -1,40 +1,34 @@
 
-var c = document.getElementById('canv');
-var $ = c.getContext('2d');
+const c = document.getElementById('canv')
+const $ = c.getContext('2d')
 
-
-var col = function(x, y, h, s, l) {
+const col = function (x, y, h, s, l) {
   // $.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-  $.fillStyle = "hsl(" + h + "," + s + "," + l + ")";
-  $.fillRect(x, y, 1,1);
+  $.fillStyle = 'hsl(' + h + ',' + s + ',' + l + ')'
+  $.fillRect(x, y, 1, 1)
 }
-var R = function(x, y, t) {
-  return( Math.abs(100 + 200*Math.sin( (x*x*Math.cos(t/40)+y*y*Math.sin(t/30))/300 )) );
-}
-
-var G = function(x, y, t) {
-  return( Math.ceil(50 + 64*Math.sin( (x*x*Math.cos(t/40)+y*y*Math.sin(t/30))/300 ) ) );
+const H = function (x, y, t) {
+  return (Math.abs(100 + 200 * Math.sin((x * x * Math.cos(t / 40) + y * y * Math.sin(t / 30)) / 300)))
 }
 
-var B = function(x, y, t) {
-  return( Math.ceil(50 + 64*Math.sin( 5*Math.sin(t/90) + ((x-100)*(x-100)+(y-100)*(y-100))/1100) ));
-}
+// const G = function (x, y, t) {
+//   return (Math.ceil(50 + 64 * Math.sin((x * x * Math.cos(t / 40) + y * y * Math.sin(t / 30)) / 300)))
+// }
 
-var t = 0;
+// const B = function (x, y, t) {
+//   return (Math.ceil(50 + 64 * Math.sin(5 * Math.sin(t / 90) + ((x - 100) * (x - 100) + (y - 100) * (y - 100)) / 1100)))
+// }
 
-var run = function() {
-  for(x=0;x<=35;x++) {
-    for(y=0;y<=35;y++) {
-      col(x, y, R(x,y,t), "25%", "85%");
+let t = 0
+
+const run = function () {
+  for (let x = 0; x <= 35; x++) {
+    for (let y = 0; y <= 35; y++) {
+      col(x, y, H(x, y, t), '25%', '85%')
     }
   }
-  t = t + 0.120;
-  window.requestAnimationFrame(run);
+  t = t + 0.120
+  window.requestAnimationFrame(run)
 }
 
-run();
-
-
-
-
-
+run()
--- hanoi/hanoi.js
+++ hanoi/hanoi.js
@@ -1,31 +1,162 @@
 // Hanoi towers
+// const _ = require('lodash')
+// const math = require('mathjs')
 /* we want an object to model the state of the hanoi tower
  * it should initialize to a certin value, (init(N)), N is the nubmer of
  * return a set of legal moves from the current state, (legalMoves())
  * make a move it if is legal, (move(move)),
  * get the current state (getState()),
- * print the current state (printState()),
- * reset the tower (tower())
+ * reset the tower (resetState())
  * and know if it is in the end state (isEndState()).
  * the state can be represented by three stacks, one for each pole (0, 1, 2)
  * each move can be represented as an ordered pair of two poles (ringStart, ringEnd), where it is assumed that you take the top ring (because you have to)
 */
+const validNumRings = (nRings) => {
+  return nRings && nRings <= 8 && nRings >= 2
+}
+const validNumTrials = (ntTrials) => {
+  return ntTrials && ntTrials <= 1000000 && ntTrials > 20
+}
+// helper functions
+const sampleFromList = (A) => {
+  return A[Math.random() * A.length >> 0]
+}
+const sampleVariance = (A) => {
+  const sampleMean = average(A)
+  const n = A.length
+  let sum = 0
+  for (let i = 0; i < n; i++) {
+    sum += Math.pow((A[i] - sampleMean), 2)
+  }
+  return sum / (n - 1.0)
+}
+const peek = (A) => {
+  return A[A.length - 1]
+}
+const average = (A) => {
+  return A.length ? A.reduce((acc, next) => acc + next) / A.length : 0
+}
+class Hanoi {
+  constructor (N) {
+    this.NUM_POLES = 3
+    this.nRings = N
+    this.initState()
+  }
+
+  initState () {
+    this.state = []
+    for (let i = 0; i < this.NUM_POLES; i++) {
+      this.state.push([])
+    }
+    for (let ring = this.nRings - 1; ring >= 0; ring--) {
+      this.state[0].push(ring)
+    }
+  }
+
+  legalMoves () {
+    const legalMoves = []
+    for (let startPole = 0; startPole < this.NUM_POLES; startPole++) {
+      for (let endPole = 0; endPole < this.NUM_POLES; endPole++) {
+        if (startPole === endPole) { continue } // not from i -> i
+        const startRing = peek(this.state[startPole])
+        if (startRing === undefined) { continue } // pole must have an object
+        const endRing = peek(this.state[endPole])
+        if (endRing !== undefined && endRing < startRing) { continue } // ring must stack onto something larger
+        legalMoves.push([startPole, endPole])
+      }
+    }
+    return legalMoves
+  }
+
+  makeMove (startPoleIndex, endPoleIndex) {
+    // console.log('making move between', startPoleIndex, endPoleIndex)
+    const startPole = this.state[startPoleIndex]
+    const endPole = this.state[endPoleIndex]
+    endPole.push(startPole.pop())
+  }
+
+  // this end state is if we require all rings to end on any pole that's not the first.
+  isEndState () {
+    // console.log('checking end state', this.state)
+    for (let i = 1; i <= this.NUM_POLES - 1; i++) {
+      // console.log('state ', i, this.state[i], this.state[i].length, this.nRings, this.state[i].length == this.nRings)
+      if (this.state[i].length === this.nRings) { return true }
+    }
+    return false
+  }
+
+  // this end state is if we require all rings to end on the last pole.
+  // by symmetry, this should increase the expected value by a factor of NUM_POLES - 1
+  // isEndState () {
+  //   return this.state[this.NUM_POLES - 1].length === this.nRings
+  // }
+  getState () {
+    return this.state
+  }
+
+  getNumRings () {
+    return this.nRings
+  }
+
+  resetState () {
+    this.initState()
+  }
+}
 // randomWalk(towers): run the towers of hanoi randomly from legal moves and return how many moves it took to reach the end state
-// getExpectedValue(tower, N): run randomWalk(N) times, returning the average value.
-// then the frontend: receiving a number N, printing (in latex?) the expected count, and getExpecgtedValue
+const randomWalk = (hanoi) => {
+  hanoi.resetState()
+  let nMoves = 0
+  while (!hanoi.isEndState()) {
+    const legalMoves = hanoi.legalMoves()
+    const move = sampleFromList(legalMoves)
+    hanoi.makeMove(move[0], move[1])
+    // console.log('state after move', hanoi.getState()[0], hanoi.getState()[1], hanoi.getState()[2])
+    nMoves++
+  }
+  return nMoves
+}
+// getSamples(tower, N): run randomWalk(N) times, returning the samples. average value.
+const getSamples = (hanoi, numTrials) => {
+  console.log(`time to build hanoi tower with ${hanoi.getNumRings()} rings.`)
+  const nMovesList = []
+  for (let i = 1; i <= numTrials; i++) {
+    if (i % 100 === 0) { console.log(`trial ${i}!`) }
+    const nMoves = randomWalk(hanoi)
+    nMovesList.push(nMoves)
+  }
+  return nMovesList
+}
+// if we require all rings to end on any pole that's not the first,
+const trueExpectedValue = (hanoi) => {
+  // from Berger and Alekseyve (2014)
+  const N = hanoi.getNumRings()
+  return (Math.pow(3, N) - 1) * (Math.pow(5, N) - Math.pow(3, N)) / (4 * Math.pow(3, (N - 1)))
+}
+// set up hanoi tower, run trials, and update the text box.
+function getHanoiSamples (numRings, numTrials) {
+  const hanoi = new Hanoi(numRings)
+  console.log(numTrials)
+  const samples = getSamples(hanoi, numTrials)
+  // samples collected!
+  console.log('Expected value estimated', average(samples))
+  document.getElementById('sample-status').innerHTML = `Success! After ${numTrials.toString()} random games of 
+    the Tower of Hanoi, the estimated expected number of moves you make is ${average(samples).toFixed(2).toString()}.`
+  console.log('True EV', trueExpectedValue(hanoi))
+  console.log('Variance of our data', sampleVariance(samples))
+  const variances = bootstrapVariance(samples, 100, 100)
+  console.log('Bootstrapped variance', variances)
+}
+// bootstrap function
+const bootstrapVariance = (samples, nBootstraps, bootSampleSize) => {
+  const resampledVars = []
+  for (let i = 0; i < nBootstraps; i++) {
+    const newSample = []
+    for (let j = 0; j < bootSampleSize; j++) {
+      newSample.push(sampleFromList(samples))
+    }
+    resampledVars.push(sampleVariance(newSample))
+  }
+  return resampledVars
+}
 // bonus: being able to visually demonstrate all the data: which iteration number, how many steps have been taken, expected value so far, etc.
 // bonus bonus: simulating a random walk really slowly and showing where on the tree you stand. also being able to draw out a tree.
-const getExpectedValue = (rings, numTrials) => {
-    console.log(`time to build hanoit tower with ${rings} rings.`);
-    for (let i = 1; i <= numTrials; i++) {
-        console.log(`trial ${i}!`);
-    }
-};
-const run = () => {
-    const rings = parseInt(prompt("how many rings you idiot"));
-    console.log(rings);
-    // init a 
-    const numTrials = parseInt(prompt("how many trials (empty for 10000)")) || 0;
-    console.log(numTrials);
-};
-run();
