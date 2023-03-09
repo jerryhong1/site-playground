cd5b65e01a2c1f35466316ec9a0adfec9454949e
Jerry Hong <jerry.hong@airtable.com>
2023-03-08 23:19:33 -0500

    raindrops!

--- react-site/pages/index.tsx
+++ react-site/pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
-export default function Home() {
+export default function Home () {
   return (
     <div className={styles.container}>
       <Head>
@@ -15,7 +16,9 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+        <h1 className={styles.title}>
+          Click <Link href="/test_page">this</Link>!
+        </h1>
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
