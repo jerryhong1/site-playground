aa546204f783742ccf65ee01ac7983b830a7838c
Jerry Hong <jerryh2@stanford.edu>
2024-03-23 16:51:52 -0400

    toggle becomes button

--- static_2024/theming.js
+++ static_2024/theming.js
@@ -1,23 +1,12 @@
-const toggleSwitch = document.querySelector('input[type="checkbox"]');
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
-console.log(currentTheme);
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
-
-    if (currentTheme === 'purple') {
-        toggleSwitch.checked = true;
-    }
-}
+const THEMES = ['ocean', 'purple']
+const themeToggle = document.getElementById('theme-toggle');
+let currentThemeIndex = parseInt(localStorage.getItem('theme-index') ? localStorage.getItem('theme-index') : '0');
+document.documentElement.setAttribute('data-theme', THEMES[currentThemeIndex]);
 
 let switchTheme = (e) => {
-  if (e.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'purple');
-    localStorage.setItem('theme', 'purple');
-  }
-  else {
-    document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('theme', 'light');
-  }    
+  currentThemeIndex = (currentThemeIndex + 1) % THEMES.length;
+  document.documentElement.setAttribute('data-theme', THEMES[currentThemeIndex]);
+  localStorage.setItem('theme-index', currentThemeIndex.toString());
 }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+themeToggle.addEventListener('click', switchTheme, false);
